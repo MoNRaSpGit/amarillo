077f0f7aefadd6f1bb1a870e96f1afd078ce7463
MoNRaSpGit <ju4nrsuarez@gmail.com>
2024-03-21 23:41:37 -0300

    dfg

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ const CameraComponent = () => {
   const videoRef = useRef(null);
   const canvasRef = useRef(null);
   const [scannedResult, setScannedResult] = useState('');
+  const [scanningMessage, setScanningMessage] = useState('No se ha escaneado ningún código de barras');
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -19,6 +20,9 @@ const CameraComponent = () => {
         const code = jsQR(imageData.data, imageData.width, imageData.height);
         if (code) {
           setScannedResult(code.data);
+          setScanningMessage('Código de barras escaneado con éxito');
+        } else {
+          setScanningMessage('No se ha escaneado ningún código de barras');
         }
       }
     }, 100);
@@ -36,13 +40,14 @@ const CameraComponent = () => {
 
   return (
     <div>
-      <h1>Camera Component</h1>
+      <h1>Camera Component2</h1>
       <button onClick={startCamera}>Start Camera</button>
-      <div style={{ width: '100%', height: '300px', border: '1px solid black' }}>
+      <div style={{ width: '100%', height: '300px', border: '1px solid black', position: 'relative' }}>
         <video ref={videoRef} autoPlay playsInline style={{ width: '100%', height: '100%' }} />
+        <canvas ref={canvasRef} style={{ position: 'absolute', top: 0, left: 0, display: 'none' }} />
       </div>
-      <p>Scanned Result: {scannedResult}</p>
-      <canvas ref={canvasRef} style={{ display: 'none' }} />
+      <p>{scanningMessage}</p>
+      {scannedResult && <p>Scanned Result: {scannedResult}</p>}
     </div>
   );
 };
